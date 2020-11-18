d7892ed88fef2880772b2a5fe3b2d08809ac3fbc
thongngo3301 <thongngo3301@gmail.com>
2020-11-19 00:41:12 +0700

    update scripts

--- scripts/add-veth-pairs.sh
+++ scripts/add-veth-pairs.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-vethA=veth$1
-vethB=veth$2
+LC='\033[1;36m'         # light cyan
+NC='\033[0m'            # no color
+echo -e "${LC}Reading config from ../config/veth-pairs.txt...${NC}"
+echo
 
-sudo ip link add name $vethA type veth peer name $vethB
-sudo ip link set dev $vethA up
-sudo ip link set dev $vethB up
-sudo ip link set $vethA mtu 9500
-sudo ip link set $vethB mtu 9500
-sudo sysctl net.ipv6.conf.$vethA.disable_ipv6=1
-sudo sysctl net.ipv6.conf.$vethB.disable_ipv6=1
+while IFS=' ' read -r col1 col2 || [ -n "$col1" ]
+do
+  vethA=veth$col1
+  vethB=veth$col2
+
+  sudo ip link add name $vethA type veth peer name $vethB
+  sudo ip link set dev $vethA up
+  sudo ip link set dev $vethB up
+  sudo ip link set $vethA mtu 9500
+  sudo ip link set $vethB mtu 9500
+  sudo sysctl net.ipv6.conf.$vethA.disable_ipv6=1
+  sudo sysctl net.ipv6.conf.$vethB.disable_ipv6=1
+done < ../config/veth-pairs.txt
---
+++ scripts/start-switch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+LC='\033[1;36m'         # light cyan
+NC='\033[0m'            # no color
+
+curr_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+project_dir="$(dirname "$curr_dir")"
+
+# compile
+cd $project_dir
+echo -e "${LC}Compiling P4 switch...${NC}"
+echo
+
+p4c -b bmv2 pm_switch.p4 -o pm_switch.bmv2
+
+# start
+cd $project_dir
+echo -e "${LC}Starting P4 switch...${NC}"
+echo
+
+veth_id_arr=()
+while IFS=' ' read -r col1 col2 || [ -n "$col1" ]
+do
+  # col2 is veth id of veth connected to switch
+  veth_id_arr+=("${col2}")
+done < config/veth-pairs.txt
+
+sudo simple_switch --interface 1@veth${veth_id_arr[0]} --interface 2@veth${veth_id_arr[1]} --interface 3@veth${veth_id_arr[2]} pm_switch.bmv2/pm_switch.json
